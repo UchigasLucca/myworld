4d487170689fc4873854d998428dd2d9fa4ab537
Lucca Moura <luccasamoura@hotmail.com>
2023-06-14 20:28:31 -0300

    adicao conversor de decimetros para a altura do pokemon

--- apicossumm.py
+++ apicossumm.py
@@ -1,1 +1,12 @@
-print ('Hello World')
+import requests
+
+pokemon_name = input('Qual nome do pokemon queres informacao?')
+pokemon = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}/'.strip().lower())
+
+pokedados = pokemon.json()
+
+if 'height' in pokedados:
+    converter_decimetros = pokedados['height'] / 10
+    print (f"ID: {pokedados['id']}\n"
+           f"Altura: {converter_decimetros} metros\n"
+           f"Numero Pokedex: {pokedados['order']}")
